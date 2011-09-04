3e0af58d54bd566bd4b083bf817264ec2e4a435f
Ulrich Sossou <sorich87@gmail.com>
2011-09-04 12:10:21 +0100

    First commit

---
+++ export-users-to-csv.php
@@ -0,0 +1,231 @@
+<?php
+/**
+ * @package Export_Users_to_CSV
+ * @version 0.2
+ */
+/*
+Plugin Name: Export Users to CSV
+Plugin URI: http://pubpoet.com/plugins/
+Description: Export Users data and metadata to a csv file.
+Version: 0.2
+Author: PubPoet
+Author URI: http://pubpoet.com/
+License: GPL2
+*/
+/*  Copyright 2011  Ulrich Sossou  (email : sorich87@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Main plugin class
+ *
+ * @since 0.1
+ **/
+class PP_EU_Export_Users {
+
+	/**
+	 * Class contructor
+	 *
+	 * @since 0.1
+	 **/
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+		add_action( 'init', array( $this, 'generate_csv' ) );
+		add_filter( 'pp_eu_exclude_data', array( $this, 'exclude_data' ) );
+	}
+
+	/**
+	 * Add administration menus
+	 *
+	 * @since 0.1
+	 **/
+	public function add_admin_pages() {
+		add_users_page( __( 'Export to CSV' ), __( 'Export to CSV' ), 'list_users', 'export-users-to-csv', array( $this, 'users_page' ) );
+	}
+
+	/**
+	 * Process content of CSV file
+	 *
+	 * @since 0.1
+	 **/
+	public function generate_csv() {
+		if ( isset( $_POST['_wpnonce-pp-eu-export-users-users-page_export'] ) ) {
+			check_admin_referer( 'pp-eu-export-users-users-page_export', '_wpnonce-pp-eu-export-users-users-page_export' );
+
+			$args = array(
+				'fields' => 'all_with_meta',
+				'role' => stripslashes( $_POST['role'] )
+			);
+
+			if ( ! empty( $args['role'] ) ) {
+				global $wp_roles;
+				$roles = array_keys( $wp_roles->role_names );
+
+				if ( ! in_array( $_POST['role'], $roles ) )
+					$args['role'] = '';
+			}
+
+			add_action( 'pre_user_query', array( $this, 'pre_user_query' ) );
+			$users = get_users( $args );
+			remove_action( 'pre_user_query', array( $this, 'pre_user_query' ) );
+
+			if ( ! $users ) {
+				$referer = add_query_arg( 'error', 'empty', wp_get_referer() );
+				wp_redirect( $referer );
+				exit;
+			}
+
+			$sitename = sanitize_key( get_bloginfo( 'name' ) );
+			if ( ! empty( $sitename ) )
+				$sitename .= '.';
+			$filename = $sitename . 'users.' . date( 'Y-m-d-H-i-s' ) . '.csv';
+
+			header( 'Content-Description: File Transfer' );
+			header( 'Content-Disposition: attachment; filename=' . $filename );
+			header( 'Content-Type: text/csv; charset=' . get_option( 'blog_charset' ), true );
+
+			$exclude_data = apply_filters( 'pp_eu_exclude_data', array() );
+
+			$first_user = reset( $users );
+			$fields = array_keys( (array) $first_user->data );
+			$headers = array();
+			foreach ( $fields as $key => $field ) {
+				if ( in_array( $field, $exclude_data ) )
+					unset( $fields[$key] );
+				else
+					$headers[] = '"' . $field . '"';
+			}
+			echo implode( ',', $headers ) . "\n";
+
+			foreach ( $users as $user ) {
+				$data = array();
+				foreach ( $fields as $field ) {
+					$user_data = (array) $user->data;
+					$value = $user_data[$field];
+					$data[] = is_array( $value ) ? '"' . serialize( $value ) . '"' : '"' . $value . '"';
+				}
+				echo implode( ',', $data ) . "\n";
+			}
+
+			exit;
+		}
+	}
+
+	/**
+	 * Content of the settings page
+	 *
+	 * @since 0.1
+	 **/
+	public function users_page() {
+		if ( ! current_user_can( 'list_users' ) )
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+?>
+
+<div class="wrap">
+	<h2><?php _e( 'Export users to a CSV file' ); ?></h2>
+	<?php
+	if ( isset( $_GET['error'] ) ) {
+		echo '<div class="updated"><p><strong>' . __( 'No user found.' ) . '</strong></p></div>';
+	}
+	?>
+	<form method="post" action="" enctype="multipart/form-data">
+		<?php wp_nonce_field( 'pp-eu-export-users-users-page_export', '_wpnonce-pp-eu-export-users-users-page_export' ); ?>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><label for"pp_eu_users_role"><?php _e( 'Role' ); ?></label></th>
+				<td>
+					<select name="role" id="pp_eu_users_role">
+						<?php
+						echo '<option value="">' . __( 'Every Role' ) . '</option>';
+						global $wp_roles;
+						foreach ( $wp_roles->role_names as $role => $name ) {
+							echo "\n\t<option value='" . esc_attr( $role ) . "'>$name</option>";
+						}
+						?>
+					</select>
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><label><?php _e( 'Date range' ); ?></label></th>
+				<td>
+					<select name="start_date" id="pp_eu_users_start_date">
+						<option value="0"><?php _e( 'Start Date' ); ?></option>
+						<?php $this->export_date_options(); ?>
+					</select>
+					<select name="end_date" id="pp_eu_users_end_date">
+						<option value="0"><?php _e( 'End Date' ); ?></option>
+						<?php $this->export_date_options(); ?>
+					</select>
+				</td>
+			</tr>
+		</table>
+		<p class="submit">
+			<input type="hidden" name="_wp_http_referer" value="<?php echo $_SERVER['REQUEST_URI'] ?>" />
+			<input type="submit" class="button-primary" value="<?php _e( 'Export' ); ?>" />
+		</p>
+	</form>
+<?php
+	}
+
+	public function exclude_data() {
+		$exclude = array( 'user_pass', 'user_activation_key', 'user_status', 'rich_editing', 'comment_shortcuts', 'admin_color', 'use_ssl', 'show_admin_bar_front', 'show_admin_bar_admin', 'wp_capabilities', 'wp_user_level', 'wp_usersettings', 'wp_usersettingstime', 'wp_dashboard_quick_press_last_post_id', 'metaboxorder_dashboard', 'screen_layout_dashboard', 'closedpostboxes_dashboard', 'metaboxhidden_dashboard', 'metaboxhidden_forum', 'closedpostboxes_forum', 'plugins_last_view', 'metaboxhidden_program', 'closedpostboxes_program', 'metaboxhidden_navmenus', 'managenavmenuscolumnshidden', 'user_level', 'user_firstname', 'user_lastname', 'user_description'  );
+
+		if ( ! is_multisite() )
+			$exclude = array_merge( $exclude, array( 'spam', 'deleted', 'primary_blog', 'source_domain' ) );
+
+		return $exclude;
+	}
+
+	public function pre_user_query( $user_search ) {
+		global $wpdb;
+
+		$where = '';
+
+		if ( ! empty( $_POST['start_date'] ) )
+			$where .= $wpdb->prepare( " AND $wpdb->users.user_registered >= %s", date( 'Y-m-d', strtotime($_POST['start_date']) ) );
+
+		if ( ! empty( $_POST['end_date'] ) )
+			$where .= $wpdb->prepare( " AND $wpdb->users.user_registered < %s", date( 'Y-m-d', strtotime( '+1 month', strtotime( $_POST['end_date'] ) ) ) );
+
+		if ( ! empty( $where ) )
+			$user_search->query_where = str_replace( 'WHERE 1=1', "WHERE 1=1$where", $user_search->query_where );
+
+		return $user_search;
+	}
+
+	private function export_date_options() {
+		global $wpdb, $wp_locale;
+
+		$months = $wpdb->get_results( "
+			SELECT DISTINCT YEAR( user_registered ) AS year, MONTH( user_registered ) AS month
+			FROM $wpdb->users
+			ORDER BY user_registered DESC
+		" );
+
+		$month_count = count( $months );
+		if ( !$month_count || ( 1 == $month_count && 0 == $months[0]->month ) )
+			return;
+
+		foreach ( $months as $date ) {
+			if ( 0 == $date->year )
+				continue;
+
+			$month = zeroise( $date->month, 2 );
+			echo '<option value="' . $date->year . '-' . $month . '">' . $wp_locale->get_month( $month ) . ' ' . $date->year . '</option>';
+		}
+	}
+}
+
+new PP_EU_Export_Users;
